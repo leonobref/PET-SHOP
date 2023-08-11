5bf9b777cf0aebc5bb671a79bedb542729d8b0eb
h4rad4 <williamharad@outlook.com>
2023-08-11 08:58:30 -0400

    Update Sales & DoSale
    o método "criarNovoArquivo" agora recebe "salesData" como parâmetro

--- Pet Shop/src/entities/Sales.java
+++ Pet Shop/src/entities/Sales.java
@@ -1,6 +1,5 @@
 package entities;
 
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import jxl.Workbook;
@@ -21,10 +20,10 @@ public class Sales
     private int customer_code;
 
     // Construtores
-    public Sales () {}
+    public Sales() {}
 
-    public Sales (String product, int product_code, float value, int quantity, String date, float sale, String customer_name,
-                  String customer_last_name, int customer_code)
+    public Sales(String product, int product_code, float value, int quantity, String date, float sale, String customer_name,
+                 String customer_last_name, int customer_code)
     {
         this.product = product;
         this.product_code = product_code;
@@ -37,80 +36,81 @@ public class Sales
         this.customer_code = customer_code;
     }
 
-    // Getters
+    // Getters e Setters
 
-    public float getSale() {return sale;}
-    public float getValue() {return value;}
-    public int getCustomer_code() {return customer_code;}
-    public int getProduct_code() {return product_code;}
-    public int getQuantity() {return quantity;}
-    public String getCustomer_last_name() {return customer_last_name;}
-    public String getCustomer_name() {return customer_name;}
-    public String getDate() {return date;}
-    public String getProduct() {return product;}
+    public float getSale() { return sale; }
+    public float getValue() { return value; }
+    public int getCustomer_code(z) { return customer_code; }
+    public int getProduct_code() { return product_code; }
+    public int getQuantity() { return quantity; }
+    public String getCustomer_last_name() { return customer_last_name; }
+    public String getCustomer_name() { return customer_name; }
+    public String getDate() { return date; }
+    public String getProduct() { return product; }
 
-    // Setters
+    public void setCustomer_code(int customer_code) { this.customer_code = customer_code; }
+    public void setCustomer_last_name(String customer_last_name) { this.customer_last_name = customer_last_name; }
+    public void setCustomer_name(String customer_name) { this.customer_name = customer_name; }
+    public void setDate(String date) { this.date = date; }
+    public void setProduct(String product) { this.product = product; }
+    public void setProduct_code(int product_code) { this.product_code = product_code; }
+    public void setSale(float sale) { this.sale = sale; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public void setValue(float value) { this.value = value; }
 
-    public void setCustomer_code(int customer_code) {this.customer_code = customer_code;}
-    public void setCustomer_last_name(String customer_last_name) {this.customer_last_name = customer_last_name;}
-    public void setCustomer_name(String customer_name) {this.customer_name = customer_name;}
-    public void setDate(String date) {this.date = date;}
-    public void setProduct(String product) {this.product = product;}
-    public void setProduct_code(int product_code) {this.product_code = product_code;}
-    public void setSale(float sale) {this.sale = sale;}
-    public void setQuantity(int quantity) {this.quantity = quantity;}
-    public void setValue(float value) {this.value = value;}
-    
-    public static void criarNovoArquivoXLS(String nomeArquivo)
+    public static void criarNovoArquivo(String nomeArquivo, Sales salesData)
     {
-        // Verificar se o arquivo já existe
-        File arquivo = new File(nomeArquivo);
-
-        if (!arquivo.exists())
-        {
-            criarNovoArquivoXLS(nomeArquivo);
-            System.out.println("Arquivo criado: " + nomeArquivo);
-        } else
-        {
-            System.out.println("Arquivo já existe: " + nomeArquivo);
-        }
-
         WritableWorkbook workbook = null;
 
         try {
-            // Criando os campos
-            workbook = Workbook.createWorkbook(new File(nomeArquivo));
-            WritableSheet sheet = workbook.createSheet("Sales", 0);
+            // Verificar se o arquivo já existe
+            File arquivo = new File(nomeArquivo);
 
-            String[] headers = {
-                    "PRODUCT", "PRODUCT CODE", "VALUE", "QUANTITY",
-                    "DATE", "SALE", "CUSTOMER NAME", "CUSTOMER LAST NAME",
-                    "CUSTOMER CODE", "0", "0"
-            };
+            if (!arquivo.exists())
+            {
+                // Criando o novo arquivo
+                workbook = Workbook.createWorkbook(arquivo);
+                WritableSheet sheet = workbook.createSheet("Sales", 0);
 
-            for (int i = 0; i < headers.length; i++) {
-                Label label = new Label(i, 0, headers[i]);
-                sheet.addCell(label);
+                String[] headers = {
+                        "PRODUCT", "PRODUCT CODE", "VALUE", "QUANTITY",
+                        "DATE", "SALE", "CUSTOMER NAME", "CUSTOMER LAST NAME",
+                        "CUSTOMER CODE", "0", "0"
+                };
+
+                for (int i = 0; i < headers.length; i++) {
+                    Label label = new Label(i, 0, headers[i]);
+                    sheet.addCell(label);
+                }
+
+                Label productLabel = new Label(0, 1, salesData.getProduct());
+                Label productCodeLabel = new Label(1, 1, String.valueOf(salesData.getProduct_code()));
+                Label valueLabel = new Label(2, 1, String.valueOf(salesData.getValue()));
+                Label quantityLabel = new Label(3, 1, String.valueOf(salesData.getQuantity()));
+                Label dateLabel = new Label(4, 1, salesData.getDate());
+                Label saleLabel = new Label(5, 1, String.valueOf(salesData.getSale()));
+                Label customerNameLabel = new Label(6, 1, salesData.getCustomer_name());
+                Label customerLastNameLabel = new Label(7, 1, salesData.getCustomer_last_name());
+                Label customerCodeLabel = new Label(8, 1, String.valueOf(salesData.getCustomer_code()));
+
+                sheet.addCell(productLabel);
+                sheet.addCell(productCodeLabel);
+                sheet.addCell(valueLabel);
+                sheet.addCell(quantityLabel);
+                sheet.addCell(dateLabel);
+                sheet.addCell(saleLabel);
+                sheet.addCell(customerNameLabel);
+                sheet.addCell(customerLastNameLabel);
+                sheet.addCell(customerCodeLabel);
+                sheet.addCell(n1);
+                sheet.addCell(n2);
+
+                workbook.write();
+                System.out.println("Arquivo criado: " + nomeArquivo);
+            } else
+            {
+                System.out.println("Arquivo já existe: " + nomeArquivo);
             }
-
-            // Inserindo os dados
-
-            Sales salesData = new Sales();
-
-            Label productLabel = new Label(0, 1, salesData.getProduct());
-            Label productCodeLabel = new Label(1, 1, salesData.getProduct_code());
-            Label value = new Label(2, 1, salesData.getValue());
-            Label quantity = new Label(3, 1, salesData.getQuantity());
-            Label date = new Label(4, 1, salesData.getDate());
-            Label sale = new Label(5, 1, salesData.getSale());
-            Label customerName = new Label(6, 1, salesData.getCustomer_name());
-            Label customerLastName = new Label(7, 1, salesData.getCustomer_last_name());
-            Label customerCode = new Label(8, 1, salesData.getCustomer_code());
-            Label n1 = new Label(9, 1, "0");
-            Label n2 = new Label(10, 1, "0");
-
-            workbook.write();
-            workbook.close();
 
             System.out.println("Dados escritos no arquivo Excel com sucesso!");
 
--- Pet Shop/src/testes/DoSale.java
+++ Pet Shop/src/testes/DoSale.java
@@ -1,6 +1,15 @@
 package testes;
-
+import entities.Sales;
 public class DoSale
 {
+    public static void main(String[] args) {
+        String nomeArquivo = "sales_data.xls"; // Nome do arquivo que você deseja criar
 
+        // Dados de exemplo
+        Sales salesData = new Sales("Sample Product", 123, 100.0f, 5, "2023-08-11", 90.0f,
+                "John", "Doe", 1);
+
+        // Criar novo arquivo XLS com os dados fornecidos
+        Sales.criarNovoArquivo(nomeArquivo, salesData);
+    }
 }
